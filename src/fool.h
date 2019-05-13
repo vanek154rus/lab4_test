a2f478b4e725f4cf39e62e9b1622f3b0dd5bead0
Ivan Prihodchenko <vanek3478989@gmail.com>
2019-05-13 14:33:48 +0700

    Fool

---
+++ src/fool.h
@@ -0,0 +1,10 @@
+#ifndef H_FOO
+#define H_FOO
+
+int PerimetrT(int a, int b, int c);
+float PerimetrC(int r, float PI);
+float SquareT(int a, int b, int c);
+float SquareC(int r, float PI);
+
+#endif
+
