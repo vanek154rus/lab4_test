5c1e25082aec37ffcef4477f46144be8ac59b7d7
Ivan Prihodchenko <vanek3478989@gmail.com>
2019-05-13 15:51:20 +0700

    Fixed

--- src/foo.h
+++ src/foo.h
@@ -4,5 +4,5 @@ int PerimetrT(int a, int b, int c);
 float PerimetrC(int r, float PI);
 float SquareT(int a, int b, int c);
 float SquareC(int r, float PI);
+void Cross(int x1, int y1, int r1, int x2, int y2, int r2);
 #endif
-
