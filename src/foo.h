df09451cd38aa715bb5c2a8598b90db7198bb8c6
Ivan Prihodchenko <vanek3478989@gmail.com>
2019-05-20 15:19:38 +0700

    Foo.h izmeneno

--- src/foo.h
+++ src/foo.h
@@ -1,8 +1,10 @@
 #ifndef H_FOO
 #define H_FOO
+
 int PerimetrT(int a, int b, int c);
 float PerimetrC(int r, float PI);
 float SquareT(int a, int b, int c);
 float SquareC(int r, float PI);
-void Cross(int x1, int y1, int r1, int x2, int y2, int r2);
+int Cross(int x1, int y1, int r1, int x2, int y2, int r2);
+
 #endif
